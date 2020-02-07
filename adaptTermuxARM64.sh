599771c87e1d8aadbdd3a0744db9d62255003357
VcotyQin <vcoty233@163.com>
2020-02-07 20:23:04 +0800

    re-undate shell script

---
+++ adaptTermuxARM64.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd
+
+# install request libs
+mkdir "libs"
+
+cd libs
+libs_base_url="https://github.com/VcotyQin/TermuxWithGradle/raw/master/libs/"
+libs=("lib7z.so" "liblog.so.0" "libaapt.so" "libpng16.so.16" "libaapt.so.0" "libprotobuf-lite.so.17" "libandroidfw.so.0" "libunwind.so.0" "libbacktrace.so.0" "libutils.so.0" "libbase.so.0" "libziparchive.so.0" "libcutils.so.0")
+
+for lib in ${libs[@]}
+do
+	wget ${libs_base_url}${lib}
+
+	if [ -e "$lib" ]
+	then
+		mv $lib /usr/lib
+	fi
+done
+
+rmdir libs
